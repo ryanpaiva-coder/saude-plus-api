4d54c7d53e62d956b0165318e3b4d8a67c1820a6
Ryan Pedro <paiva.code@gmail.com>
2025-03-30 22:00:31 -0300

    Initial commit

---
+++ src/main/java/br/com/projeto/saude_plus/SaudePlusApplication.java
@@ -0,0 +1,13 @@
+package br.com.projeto.saude_plus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaudePlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaudePlusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/projeto/saude_plus/SaudePlusApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projeto.saude_plus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaudePlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
