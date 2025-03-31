3e3029bf9fbf4429309d53bf29165defc4a51e23
Rafael Souza <rafael.souza29184@gmail.com>
2025-03-31 13:47:02 -0300

    criar serviço UserService

---
+++ src/main/java/br/com/projeto/saude_plus/domain/service/UserService.java
@@ -0,0 +1,45 @@
+package br.com.projeto.saude_plus.domain.service;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.projeto.saude_plus.domain.model.User;
+import br.com.projeto.saude_plus.domain.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Transactional
+    public User register(final User user) {
+        return userRepository.save(user);
+    }
+
+    @Transactional
+    public User update(final User user, UUID userId) {
+
+        User findUser = search(userId);
+        findUser.setName(user.getName());
+
+        return userRepository.save(findUser);
+    }
+
+    @Transactional
+    public User search(final UUID userId) {
+        return userRepository
+                .findById(userId)
+                .orElseThrow(() -> new EntityNotFoundException("User not found"));
+    }
+
+    @Transactional
+    public List<User> list() {
+        return userRepository.findAll();
+    }
+}
