df046e7f205e0dd0b649f1c71fcf117547c0f981
Rafael Souza <rafael.souza29184@gmail.com>
2025-04-01 09:58:24 -0300

    adicionar validações para o campo CRM na classe Medic

--- src/main/java/br/com/projeto/saude_plus/domain/model/Medic.java
+++ src/main/java/br/com/projeto/saude_plus/domain/model/Medic.java
@@ -3,6 +3,8 @@ package br.com.projeto.saude_plus.domain.model;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,6 +20,8 @@ import lombok.experimental.SuperBuilder;
 public class Medic extends User{
 
     @NotNull
+    @Size(min = 5, max = 20, message = "O CRM deve ter entre 5 e 20 caracteres.")
+    @Pattern(regexp = "^[A-Za-z0-9]+$", message = "O CRM deve conter apenas letras e números.")
     @Column(unique = true)
     private String crm;
 
