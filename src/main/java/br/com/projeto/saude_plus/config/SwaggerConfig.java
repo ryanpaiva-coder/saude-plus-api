3c16530770276bb7e30db8c5111de87751b46892
Ryan Pedro <paiva.code@gmail.com>
2025-04-03 11:13:17 -0300

    Adicionando Swagger para documentação da API

---
+++ src/main/java/br/com/projeto/saude_plus/config/SwaggerConfig.java
@@ -0,0 +1,15 @@
+package br.com.projeto.saude_plus.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().info(new Info().title("Saude Plus API").version("1.0").description("API para marcar e conferir consultas médicas."));
+    }
+}
