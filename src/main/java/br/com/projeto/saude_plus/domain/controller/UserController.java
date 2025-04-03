7bf27bf6d209a704c82c01fe069fe8d3da37d5b9
Ryan Pedro <paiva.code@gmail.com>
2025-04-03 10:10:02 -0300

    criando o endpoint de cadastro de usuario

---
+++ src/main/java/br/com/projeto/saude_plus/domain/controller/UserController.java
@@ -0,0 +1,25 @@
+package br.com.projeto.saude_plus.domain.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.projeto.saude_plus.domain.model.User;
+import br.com.projeto.saude_plus.domain.service.UserService;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> registerUser(@RequestBody User user) {
+        User newUser = userService.register(user);
+        return ResponseEntity.ok(newUser);
+    }
+}
