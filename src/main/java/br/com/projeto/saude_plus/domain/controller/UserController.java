31346372589454201bce5604ae6584d005f94eaf
Ryan Pedro <paiva.code@gmail.com>
2025-04-03 10:18:23 -0300

    criando endpoint de listagem de usuarios

--- src/main/java/br/com/projeto/saude_plus/domain/controller/UserController.java
+++ src/main/java/br/com/projeto/saude_plus/domain/controller/UserController.java
@@ -1,7 +1,10 @@
 package br.com.projeto.saude_plus.domain.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,5 +24,11 @@ public class UserController {
     public ResponseEntity<User> registerUser(@RequestBody User user) {
         User newUser = userService.register(user);
         return ResponseEntity.ok(newUser);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> listUsers() {
+        List<User> users = userService.list();
+        return ResponseEntity.ok(users);
     }
 }
