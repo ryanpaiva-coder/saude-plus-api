f267416ba11ad8567b0a0c9347e06e2828685ef3
Rafael Souza <rafael.souza29184@gmail.com>
2025-05-22 15:13:03 -0300

    Adicionar classe Role com mapeamento JPA e atributos id e nome

---
+++ src/main/java/br/com/projeto/saude_plus/domain/model/Role.java
@@ -0,0 +1,22 @@
+package br.com.projeto.saude_plus.domain.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Role")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_role")
+    private Integer id;
+
+    @Column(nullable = false, unique = true, length = 32)
+    private String nome;
+}
