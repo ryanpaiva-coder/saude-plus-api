963b08d63af16733ba8c8dcbe0a22b4954268e84
Rafael Souza <rafael.souza29184@gmail.com>
2025-06-02 15:49:55 -0300

    Adicionar script de inserção de especialidades médicas no banco de dados

---
+++ src/main/resources/db/migration/V3__inserir_especialidades.sql
@@ -0,0 +1,4 @@
+INSERT INTO especialidade (nome, descricao) VALUES
+('Cardiologia', 'Especialidade médica que se ocupa do diagnóstico e tratamento de doenças do coração e do sistema circulatório.'),
+('Dermatologia', 'Especialidade médica que se dedica ao estudo, diagnóstico e tratamento das doenças da pele, cabelos e unhas.'),
+('Pediatria', 'Especialidade médica que cuida da saúde de crianças e adolescentes, desde o nascimento até os 18 anos.'),
