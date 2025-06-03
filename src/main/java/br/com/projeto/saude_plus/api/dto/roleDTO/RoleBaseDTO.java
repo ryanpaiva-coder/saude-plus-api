3b0269ca8d724761ecbf371501753c8da544a469
Rafael Souza <rafael.souza29184@gmail.com>
2025-06-03 11:46:51 -0300

    Adicionar classes DTO para gerenciamento de funções

---
+++ src/main/java/br/com/projeto/saude_plus/api/dto/roleDTO/RoleBaseDTO.java
@@ -0,0 +1,13 @@
+package br.com.projeto.saude_plus.api.dto.roleDTO;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RoleBaseDTO {
+
+    @NotNull
+    private Long id;
+}
