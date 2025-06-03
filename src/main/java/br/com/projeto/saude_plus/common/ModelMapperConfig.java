87b7e63b367eca951a6d2fdae34b7985e28b9e17
Rafael Souza <rafael.souza29184@gmail.com>
2025-06-03 14:28:00 -0300

    Adicionar configuração do ModelMapper para mapeamento de objetos

---
+++ src/main/java/br/com/projeto/saude_plus/common/ModelMapperConfig.java
@@ -0,0 +1,15 @@
+package br.com.projeto.saude_plus.common;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+}
