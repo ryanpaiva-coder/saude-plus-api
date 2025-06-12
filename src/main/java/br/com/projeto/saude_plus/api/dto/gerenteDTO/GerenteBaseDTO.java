5ee31ee374124e9d59cdcdf2f822fddda6932cbe
Ryan Pedro <paiva.code@gmail.com>
2025-06-12 08:33:31 -0300

    Adiciona DTOs e assembler para gerente

--- src/main/java/br/com/projeto/saude_plus/api/dto/gerenteDTO/GerenteBaseDTO.java
+++ src/main/java/br/com/projeto/saude_plus/api/dto/gerenteDTO/GerenteBaseDTO.java
@@ -1,5 +1,13 @@
 package br.com.projeto.saude_plus.api.dto.gerenteDTO;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class GerenteBaseDTO {
-    
+
+    @NotNull
+    private Long id;
 }
