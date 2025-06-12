ec1952f4fcebc6fd4111ac11b6ec1609df715d84
Ryan Pedro <paiva.code@gmail.com>
2025-06-12 08:23:14 -0300

    Adiciona DTOs base, input e output para gerente

---
+++ src/main/java/br/com/projeto/saude_plus/api/dto/gerenteDTO/GerenteBaseDTO.java
@@ -0,0 +1,5 @@
+package br.com.projeto.saude_plus.api.dto.gerenteDTO;
+
+public class GerenteBaseDTO {
+    
+}
---
+++ src/main/java/br/com/projeto/saude_plus/assembler/GerenteAssembler.java
@@ -0,0 +1,5 @@
+package br.com.projeto.saude_plus.assembler;
+
+public class GerenteAssembler {
+    
+}
